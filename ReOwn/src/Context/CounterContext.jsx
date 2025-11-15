821c2ab8adb325c98382fae26491c47b5ef8000b
Mariam.M <mariammohammeds0712@gmail.com>
2025-11-15 11:50:56 +0200

    Add CounterContextProvider for counter state
    Implement CounterContextProvider with state management.

--- ReOwn/src/Context/CounterContext.jsx
+++ ReOwn/src/Context/CounterContext.jsx
@@ -1,1 +1,18 @@
+// counterContext.jsx
+
+import { createContext, useState } from "react";
+export let CounterContext = createContext();
+
+export default function CounterContextProvider() {
+  const [counter, setCounter] = useState(0);
+
+  function changeCounter() {
+    setCounter(Math.random());
+  }
+
+  return (
+    <CounterContext.Provider
+      value={{ counter, changeCounter }} >
+    </CounterContext.Provider>
+  );
 
