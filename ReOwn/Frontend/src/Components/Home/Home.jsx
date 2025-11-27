fe058700bd134d6ca9cdd19debb6c09221167c8b
Somaya Medhat <somayamedhat179>
2025-11-27 15:49:57 +0200

    Add Categories, Products and Details pages + link navigation between them  by Somaya

--- ReOwn/Frontend/src/Components/Home/Home.jsx
+++ ReOwn/Frontend/src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Categories from './Categories/Categories'
+import Categories from '../Categories/Categories'
 import ProductRecentlyAdded from './ProductRecentlyAdded/ProductRecentlyAdded'
 export default function Home() {
   return (
---
+++ ReOwn/Frontend/src/Components/Home/ProductRecentlyAdded/ProductRecentlyAdded.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductRecentlyAdded = () => {
+  return (
+    <div>
+      <h2>Recently Added Products</h2>
+     
+    </div>
+  );
+};
+
+export default ProductRecentlyAdded;
