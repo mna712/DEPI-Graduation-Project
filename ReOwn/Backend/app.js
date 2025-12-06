3b74019d09a928069a9b51b448b0fd48c87eb0f2
mna712 <mennaelgharabawii@gmail.com>
2025-12-06 22:16:59 +0200

    fix: fix CORS Problem

--- ReOwn/Backend/app.js
+++ ReOwn/Backend/app.js
@@ -8,8 +8,12 @@ import path from "path"
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5174/"],
+    credentials: true,
+  })
+);app.use(express.json());
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/api", Router);
 dbConnect()
