98f5eaa5bd94c66aff0d85bf8514bad1328c5b4d
mna712 <mennaelgharabawii@gmail.com>
2025-12-07 14:01:37 +0200

    fix: fix cloudinary problem

--- ReOwn/Backend/app.js
+++ ReOwn/Backend/app.js
@@ -4,21 +4,19 @@ import express from "express";
 import cors from "cors";
 import dbConnect from "./config/dbConnection.js";
 import Router from "./routes/index.js";
-import path from "path";
+
 const app = express();
 const PORT = process.env.PORT;
-app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", Router);
+
 dbConnect()
-  .then((msg) => {
-    console.log(msg);
-    app.listen(PORT, () => {
-      console.log("Server is running on port", PORT);
-    });
+  .then(() => {
+    app.listen(PORT, () => console.log("Server running on", PORT));
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch(console.log);
+
